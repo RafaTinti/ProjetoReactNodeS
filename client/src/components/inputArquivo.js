7b8360011c3235d95a22262c9a5cd215bf04c8d3
Rafael Tinti <fael145@gmail.com>
2023-09-08 01:59:52 -0300

    parse do csv e adicao de bibiliotecas

--- client/src/components/inputArquivo.js
+++ client/src/components/inputArquivo.js
@@ -1,19 +1,35 @@
 import React, {Fragment, useState} from "react";
 import axios from "axios";
+import Papa from "papaparse";
 
 function InputAquivo(){
 
     const onSubmitForm = async e => {
         e.preventDefault();
+        e.stopPropagation();
         try {
-            var formData = new FormData();
-            var file = document.querySelector("#fileInput");
-            formData.append("file", file.files[0]);
-            await axios.post("http://localhost:5000", formData, {
-                headers: {
-                  'Content-Type': 'multipart/form-data',
-                }
-            });
+            let file = document.querySelector("#fileInput").files[0];
+            console.log(file);
+            if(file){
+                Papa.parse(file, {
+                    header: true,
+                    skipEmptyLines: true,
+                    complete: function(results){
+                        console.log("finised:", results.data);
+                        axios.post("http://localhost:5000/produtos", results.data);
+                    }
+                });
+            }
+            // let formData = new FormData();
+            // let file = document.querySelector("#fileInput");
+            // console.log(file.files[0]);
+            // formData.append("file", file.files[0]);
+            // console.log('>> formData >>', formData);
+            // await axios.post("http://localhost:5000/produtos", file.files[0], {
+            //     headers: {
+            //       'Content-Type': "multipart/form-data",
+            //     }
+            // });
             console.log("im running");
         } catch (err) {
             console.log(err.message);
--- server/index.js
+++ server/index.js
@@ -11,7 +11,6 @@ app.use(cors());
 
 //ROTAS
 
-
 app.use("/", userRoutes)
 //rotas de produtos
 app.use("/produtos", produtosRoutes);
